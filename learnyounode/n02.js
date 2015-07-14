92cdc2f7dad74a3c39bb99e32b878b1cca66c3f0
Guilherme <guilhermecruz@gmail.com>
2015-07-14 13:53:11 -0300

    lapidada nos parametros

--- learnyounode/n02.js
+++ learnyounode/n02.js
@@ -3,6 +3,6 @@ Imprime a soma de todos os argumentos passados pelo terminal
 */
 var sum = 0;
 for(var a=2;a<process.argv.length;a++){
-	sum += Number(process.argv[a]);
+	sum += (Number(process.argv[a]) || 0);
 }
 console.log(sum);
